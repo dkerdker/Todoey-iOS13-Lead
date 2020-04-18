fcb744a321c74266289a7ff9043d0e749b576022
Dee Ker <dker95@icloud.com>
2020-04-18 20:26:37 +0800

    Delete readme

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Dee Ker Khoo on 05/04/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
